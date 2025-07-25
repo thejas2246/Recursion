cdc93b36ddbae900af14d1da31290119c8df7a38
Thejas A <thejas2246@gmail.com>
2025-07-25 11:04:53 +0530

    Implement fibonacci using recursion

--- fibonacci.js
+++ fibonacci.js
@@ -2,8 +2,13 @@ function fibs(val) {
     let first = 0;
     let second = 1;
     let array = [];
-    array.push(first);
-    for (let i = 1; i < val; i++) {
+    if (val === 1) {
+        array.push(first);
+    } else if (val >= 2) {
+        array.push(first);
+        array.push(second);
+    }
+    for (let i = 2; i < val; i++) {
         let result = first + second;
         first = second;
         second = result;
@@ -12,4 +17,14 @@ function fibs(val) {
     return array;
 }
 
-console.log(fibs(5));
+console.log(fibs(6));
+
+function fibsRec(val, result = [0, 1]) {
+    if (val <= 0) return [];
+    if (val === 1) return [0];
+    if (val === 2) return result;
+    result.push(result[result.length - 1] + result[result.length - 2]);
+    return fibsRec(val - 1, result);
+}
+
+console.log(fibsRec(6));
